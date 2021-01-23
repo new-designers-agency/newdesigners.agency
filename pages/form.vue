bb9034774e7dd8305d5ba80ceb220df2e1d9f151
waysagency <76130749+waysagency@users.noreply.github.com>
2021-01-23 15:20:21 +0100

    Update form.vue

--- pages/form.vue
+++ pages/form.vue
@@ -18,6 +18,54 @@
       <!-- Project info -->
       <section>
         <div class="container max-w-3xl">
+          <h2 class="text-left mb-1">What kind of project is it?</h2>
+
+          <!-- Categories -->
+          <div id="categories" class="form-group">
+            <span>Please check all categories that may apply.</span>
+
+            <div class="flex flex-col flex-wrap sm:flex-row">
+              <div
+                class="form-check"
+                v-for="category in categories"
+                :key="category"
+              >
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  :name="category"
+                  :id="category"
+                />
+
+                <label class="form-check-label" :for="category">
+                  {{ category | capitalize }}
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <!-- Language -->
+          <div class="form-group">
+            <span>Project language preference.</span>
+
+            <div
+              class="form-check"
+              v-for="language in languages"
+              :key="language"
+            >
+              <input
+                class="form-check-input"
+                type="radio"
+                name="language"
+                :id="language | lowercase"
+              />
+
+              <label class="form-check-label" :for="language | lowercase">
+                {{ language }}
+              </label>
+            </div>
+          </div>
+
           <!-- Project title -->
           <div class="form-group">
             <label for="project-title">Project title *</label>
@@ -49,3 +97,36 @@
     </form>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      categories: [
+        "internship",
+        "graduation-project",
+        "freelance",
+        "paid",
+        "unpaid",
+        "research",
+        "design",
+        "development"
+      ],
+      languages: ["English or Dutch", "Only English", "Only Dutch"]
+    };
+  },
+  filters: {
+    capitalize: function(value) {
+      if (!value) return "";
+      value = value.toString();
+      value = value.charAt(0).toUpperCase() + value.slice(1);
+      return value.replace("-", " ");
+    },
+    lowercase: function(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.replace(/\s+/g, "-").toLowerCase();
+    }
+  }
+};
+</script>
