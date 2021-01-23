776e5e46ce6bb48270127c4e116025fbf896c68b
waysagency <76130749+waysagency@users.noreply.github.com>
2021-01-23 13:15:32 +0100

    Create form.vue

---
+++ pages/form.vue
@@ -0,0 +1,24 @@
+<template>
+  <form
+    name="contactus"
+    action="/thanks"
+    method="post"
+    netlify
+    netlify-honeypot="bot-field"
+  >
+    <input type="hidden" name="form-name" value="contactus" />
+    <div>
+      <label for="name">Name:</label>
+      <input type="text" name="name" required />
+    </div>
+    <div>
+      <input type="email" name="email" required />
+      <label for="email">Email:</label>
+    </div>
+    <div>
+      <textarea name="message" required></textarea>
+      <label for="message">Message:</label>
+    </div>
+    <button type="submit" value="Send message">Send</button>
+  </form>
+</template>
