8288ea5891e32aaa4d6aa6d24071574f3bbf9699
waysagency <76130749+waysagency@users.noreply.github.com>
2021-01-23 23:53:58 +0100

    Update

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -1,5 +1,40 @@
 <template>
-  <article>
-    <Project />
-  </article>
+  <main>
+    <!-- Hero -->
+    <section id="hero">
+      <div class="container">
+        <h1>{{ project.title }}</h1>
+      </div>
+    </section>
+
+    <article>
+      <p>{{ formatDate(project["publish-date"]) }}</p>
+      <p>{{ project.categories }}</p>
+      <p>{{ project.languages }}</p>
+      <Project />
+    </article>
+  </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      project: []
+    };
+  },
+
+  async fetch() {
+    this.project = await this.$content(this.$route.path).fetch();
+    console.log(this.$route.path);
+    console.log(this.project);
+  },
+
+  methods: {
+    formatDate(date) {
+      const options = { year: "numeric", month: "long", day: "numeric" };
+      return new Date(date).toLocaleDateString("nl", options);
+    }
+  }
+};
+</script>
