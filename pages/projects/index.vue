9afd751e2f408706193e576535c1db7470166e39
waysagency <76130749+waysagency@users.noreply.github.com>
2021-01-24 13:47:47 +0100

    Update projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -9,7 +9,55 @@
 
     <section>
       <div class="container max-w-3xl">
-        <Projects />
+        <p v-if="$fetchState.pending">Fetching projects...</p>
+        <p v-else-if="$fetchState.error">An error occurred :(</p>
+        <ul v-else id="projects" class="grid grid-cols-1 gap-2">
+          <li
+            v-for="project of projects"
+            :key="project[`slug`]"
+            class="project"
+          >
+            <NuxtLink
+              :to="`${project.dir}/${project[`slug`]}`"
+              class="clickable-card"
+            >
+              <div :to="`${project.dir}/${project[`slug`]}`" class="card">
+                <div class="card-body">
+                  <h3 class="card-title">{{ project[`title`] }}</h3>
+                  <h5 class="card-subtitle">{{ project[`company-name`] }}</h5>
+                  <p
+                    class="card-text overflow-hidden overflow-ellipsis whitespace-nowrap max-h-3"
+                  >
+                    {{ project[`project-description`] }}
+                  </p>
+                </div>
+                <div class="card-footer flex">
+                  <p>
+                    <span class="publish-date">
+                      {{ formatDate(project[`publish-date`]) }}
+                    </span>
+                    -
+                    <span class="duration">
+                      {{ project[`estimated-duration`] }}
+                    </span>
+                    -
+                    <template
+                      v-for="(language, name, index) in project[`languages`]"
+                    >
+                      <span
+                        v-if="language === true"
+                        :key="index"
+                        class="languages"
+                      >
+                        {{ capitalize(name) }}
+                      </span>
+                    </template>
+                  </p>
+                </div>
+              </div>
+            </NuxtLink>
+          </li>
+        </ul>
       </div>
     </section>
   </main>
@@ -17,16 +65,30 @@
 
 <script>
 export default {
-  head: {
-    title: "Projects",
-    meta: [
-      {
-        hid: "description",
-        name: "description",
-        content: "New Designers Agency projects"
-      },
-      { hid: "robots", name: "robots", content: "noindex" }
-    ]
+  data() {
+    return {
+      projects: []
+    };
+  },
+
+  async fetch() {
+    this.projects = await this.$content("projects")
+      .limit(5)
+      .fetch();
+    console.log(this.projects);
+  },
+
+  methods: {
+    formatDate(date) {
+      const options = { year: "numeric", month: "short", day: "numeric" };
+      return new Date(date).toLocaleDateString("nl", options);
+    },
+    capitalize(value) {
+      if (!value) return "";
+      value = value.toString();
+      value = value.charAt(0).toUpperCase() + value.slice(1);
+      return value.replace(/-/g, " ");
+    }
   }
 };
 </script>
